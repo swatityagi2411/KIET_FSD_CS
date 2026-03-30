fde71b1459dbb2b74962773a932a5d5ef1a32c20
Swati Tyagi <swatityagi.dgi@gmail.com>
2026-03-30 14:06:28 +0530

    Pagination

--- NewsApp/src/components/News.jsx
+++ NewsApp/src/components/News.jsx
@@ -3,18 +3,21 @@ import NewsCard from "./NewsCard";
 
 export default function News() {
   const [news, setNews] = useState([]);
-  const [search,setSearch]=useState("tesla");
+  const [search, setSearch] = useState("tesla");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-  //state in pagination
-  const [currentPage,setCurrentPage]=useState(1);
-  const itemPerPage=6;
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 6;
 
   function fetchdata() {
     setLoading(true);
+    setError("");
+    setCurrentPage(1);
 
     fetch(
-      "https://newsapi.org/v2/everything?q=${search}&from=2026-02-23&sortBy=publishedAt&apiKey=dd87f40200304e7289285277a13c8071")
+      "https://newsapi.org/v2/everything?q=tesla&from=2026-02-28&sortBy=publishedAt&apiKey=dd87f40200304e7289285277a13c8071"
+    )
       .then((res) => res.json())
       .then((data) => {
         if (data.status !== "ok") {
@@ -34,30 +37,60 @@ export default function News() {
     fetchdata();
   }, []);
 
-const lastIndex=itemPerPage*currentPage;
-const firstIndex=lastIndex-itemPerPage;
-const currentNews=news.slice(firstIndex,lastIndex);
-const totalPages=math.ceil(news.length/itemPerPage);
+  const lastIndex = itemPerPage * currentPage;
+  const firstIndex = lastIndex - itemPerPage;
+  const currentNews = news.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(news.length / itemPerPage);
 
- 
- if (loading) return <h1>Loading....</h1>;
+  if (loading) return <h1>Loading....</h1>;
   if (error) return <h1>{error}</h1>;
-  if (news.length === 0) return <h2>No News Found </h2>;
+  if (news.length === 0) return <h2>No News Found</h2>;
 
   return (
-    <div >
-      <h3 > Latest News</h3>
-      <input 
-  type="text"
-  placeholder="Search news..."
-  value={search}
-  onChange={(e) => setSearch(e.target.value)}
-/>
+    <div>
+      <h3>Latest News</h3>
 
-<button onClick={fetchdata}>Search</button>
+      <input
+        type="text"
+        placeholder="Search news..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <button onClick={fetchdata}>Search</button>
+
       {currentNews.map((item, index) => (
         <NewsCard key={index} news={item} />
       ))}
+
+      <div style={{ marginTop: "20px" }}>
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+           Prev
+        </button>
+
+        {[...Array(totalPages)].map((_, i) => (
+          <button
+            key={i}
+            onClick={() => setCurrentPage(i + 1)}
+            style={{
+              margin: "5px",
+              fontWeight: currentPage === i + 1 ? "bold" : "normal",
+            }}
+          >
+            {i + 1}
+          </button>
+        ))}
+
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next 
+        </button>
+      </div>
     </div>
   );
 }
