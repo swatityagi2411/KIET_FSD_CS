783675b1ee10973e0dbfad810253edae516247bf
Swati Tyagi <swatityagi.dgi@gmail.com>
2026-04-13 16:31:18 +0530

    useReducer added

--- context-api-demo/src/App.jsx
+++ context-api-demo/src/App.jsx
@@ -3,26 +3,22 @@ import{ThemeContext,UserContext,LangContext} from './context';
 
 const App=()=>{
 
-  const {theme,toggleTheme}=useContext(ThemeContext);
+  /*const {theme,toggleTheme}=useContext(ThemeContext);
   const {user,setUser}=useContext(UserContext);
-  const {lang,setLang}=useContext(LangContext);
+  const {lang,setLang}=useContext(LangContext);*/
+
+     const {state,dispatch}=useContext(ThemeContext);
+    const {theme,user,lang}=state;
 
   return(
-   
-<div
-  style={{
-    background: theme === "light" ? "#fff" : "#333",
-    color: theme === "light" ? "#000" : "#fff",
-    padding: "20px",
-  }}
->
+    <div style={{background:theme==="light"?"#fff":"#333"}}>
       <p>Theme:{theme}</p>
       <p>User:{user.name}</p>
       <p>Lang:{lang}</p>
 
-      <button onClick={toggleTheme}>ThemeChange</button>
-      <button onClick={()=>setLang("en")}>en</button>
-      <button onClick={()=>setLang("fr")}>fr</button>
+      <button onClick={()=>dispatch({type:"TOGGLE_THEME"})}>ThemeChange</button>
+      <button onClick={()=>dispatch({type:"USE_LANG"})}>en</button>
+      <button onClick={()=>dispatch({type:"USE_LANG"})}>fr</button>
 
 
 
--- context-api-demo/src/Appprovider.jsx
+++ context-api-demo/src/Appprovider.jsx
@@ -1,19 +1,64 @@
-import {useState} from 'react';
+//import {useState} from 'react';//no need with reducer
+import { useContext, useReducer } from 'react';
 import{ThemeContext,UserContext,LangContext} from './context';
 
-const Appprovider=({children})=>{
-    const [theme,setTheme]=useState("light");
-    const [user,setUser]=useState({name:"guest"})
-    const [lang,setLang]=useState("en");
+//2. create reducer function
 
-    const toggleTheme=()=>
+function reducer(state,action){
+    switch(action.type)
+    {
+        case "TOGGLE_THEME":
+            return{...state,
+                theme:state.theme==="light"?"dark":"light",
+            }
+
+        case "SET_USER":
+            return{
+                ...state,
+                user:action.payload,
+            }
+
+        case "SET_LANG":
+            return{
+                ...state,
+                lang:action.payload,
+            }
+
+        default:
+            return state;
+
+    }
+}
+
+
+const Appprovider=({children})=>{
+    
+//Updated with reducer
+    //1.create common state object
+
+    const initialState={
+        theme:"light",
+        user:{name:"Guest"},
+        lang:"en"
+    }
+
+    /*const [theme,setTheme]=useState("light");
+    const [user,setUser]=useState({name:"guest"})
+    const [lang,setLang]=useState("en");*/
+
+    //3. create single state object with dispatch 
+ const [state,dispatch]=useReducer(reducer,initialState)
+
+  /*  const toggleTheme=()=>
     {
         setTheme(prev=> prev==="light"?"dark":"light");
-    }
-return(
-<ThemeContext.Provider value={{theme,toggleTheme}}>
-    <UserContext.Provider value={{user,setUser}}>
-        <LangContext.Provider value={{lang,setLang}}>
+    }*/
+
+        //instead of passing seperate state and its handler or setter we are passing common state object and dipatcher
+        return(
+<ThemeContext.Provider value={{state,dispatch}}>
+    <UserContext.Provider value={{state,dispatch}}>
+        <LangContext.Provider value={{state,dispatch}}>
             {children}
         </LangContext.Provider>
 
